4cd5c6d04ddc789a6ae6ecf2576da5f490407a15
mrstruijk <mrstruijk@gmail.com>
2026-02-23 09:32:18 +0100

    Cantor tapping term to 150

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -7,6 +7,9 @@
 #define SERIAL_USART_TX_PIN B6     // USART TX pin
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
 
+
+#define TAPPING_TERM 150
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
