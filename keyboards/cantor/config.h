3d9932022543ae72442e50b9d806dcce7535aa8c
mrstruijk <mrstruijk@gmail.com>
2026-03-13 09:20:22 +0100

    cantor taps

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -7,21 +7,4 @@
 #define SERIAL_USART_TX_PIN B6     // USART TX pin
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
 
-
 #define TAPPING_TERM 150
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
