6e6584edd1e4fbf28fbc1af11a8f51cad8c00c63
mrstruijk <mrstruijk@gmail.com>
2026-03-13 10:42:16 +0100

    updated cantor to accomodate 42 keys

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -8,3 +8,10 @@
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
 
 #define TAPPING_TERM 150
+
+// Mouse speed settings - slower default, faster with modifier
+#define MOUSEKEY_DELAY 20           // Delay before mouse starts moving (lower = faster start)
+#define MOUSEKEY_INTERVAL 20        // Time between mouse movements (lower = faster)
+#define MOUSEKEY_MAX_SPEED 5        // Max speed (default is 10, we're using 5 for slower)
+#define MOUSEKEY_ACCEL_ENABLE       // Enable acceleration
+#define MOUSEKEY_ACCELERATED_MAX 10 // Max speed when accelerated (faster option)
